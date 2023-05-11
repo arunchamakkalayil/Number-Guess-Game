855a2d5ac22c951e0b32f1e05cc639e1b0d204a9
arunchamakkalayil <strangerthings369369@gmail.com>
2023-05-11 12:43:04 +0530

    bugs fixed

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-let random = Math.random() * 11
-let final = Math.floor(random)
+
 let number;
 
 function start(){
-    
+    let random = Math.random() * 101
+    let final = Math.floor(random)
+    console.log(final)
     do{
-        number = prompt('Enter a number between 0 - 10')
+        number = prompt('Enter a number between 0 - 100')
         if(number==final){
-            alert('You Won')
-            document.getElementById('message').innerText= 'Congratulations'
+            alert('Congratulations, You Won')
+           
         }else{
             alert('Wrong Guess')
         }
     }while(number != final)
 }
 
-start()
